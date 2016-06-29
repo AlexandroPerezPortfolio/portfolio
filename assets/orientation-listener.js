1f4071570e2012822e5486306a2e75e7c5924d96
Alexandro Perez <apgfriend@gmail.com>
2016-06-29 04:48:26 +0100

    add fix v2 for orientation changes

--- assets/orientation-listener.js
+++ assets/orientation-listener.js
@@ -6,37 +6,53 @@
 
 /*  Requirements: jQuery */
 
-function handleOrientationChange(mql) {
-  // set a small timeout to give time to the browswer to change, and then get
-  // the height. NOTE: without this, it doesn't work in chrome devtools when testing
-  // orientation changes. Have to check on actual devices without this fix.
-  /*setTimeout(function() { /**/
-    // always do something
-    // default height.
-    var height = "100vh";
-    if (isMobileDevice()) {
-      height = $(window).height().toString() + "px";;
-    }
-    $("header").css("height", height);
+var Device = function() {
+  // public properties
+  this.width = screen.width;
+  this.height = screen.height;
+  this.barHeight = this.height - window.innerHeight;
 
-    // left as it is for reference only
-    if (mql.matches) {
-      // Viewport in portrait orientation
-      // do nothing
-    } else {
-      // Viewport in landscape orientation
-      // do nothing
-    }
-  /*}, 0); /**/
+  // private variables
+  var orientation = getOrientation(this.width, this.height); // "landscape" or "portrait"
+  var onOrientationChangeFunction = function(){}; // function to execute when orientation changes
 
+  // privileged methods
+  this.getOrientation = function() {
+    return orientation;
+  };
+
+  this.addOrientationChangeListener = function(fn) {
+    onOrientationChangeFunction = fn;
+  };
+
+  this.updateState = function() {
+    this.width = screen.width;
+    this.height = screen.height;
+    if (orientation !== getOrientation(this.width, this.height)) {
+      orientation = getOrientation(this.width, this.height);
+      onOrientationChange(onOrientationChangeFunction);
+    }
+  };
+
+  // private methods
+  function onOrientationChange(fn) {
+    fn();
+  }
+
+  // construcor code (always executes at instantiation)
+  $(window).resize(function(){
+    device.updateState();
+  });
+};
+
+// Useful functions
+function getOrientation(width, height) {
+  if (width > height){
+    return "landscape";
+  } else {
+    return "portrait";
+  }
 }
-
-//  mql: media query listener
-var mql = window.matchMedia("(orientation: portrait)");
-mql.addListener(handleOrientationChange);
-handleOrientationChange(mql);
-
-// checks if devices is an Android, iPhone or iPad
 function isMobileDevice() {
   var ua = navigator.userAgent.toLowerCase();
   var android = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
@@ -44,3 +60,14 @@ function isMobileDevice() {
   var iPad = ua.indexOf("ipad") > -1; //&& ua.indexOf("mobile");
   return android || iPhone || iPad;
 }
+
+var device = new Device();
+
+device.addOrientationChangeListener(function() {
+  var height = "100vh";
+  if (isMobileDevice()) {
+    console.log(++counter, "is mobile deviced and changed orientation");
+    height =  (device.height - device.barHight).toString() + "px";;
+  }
+  $("header").css("height", height);
+});
