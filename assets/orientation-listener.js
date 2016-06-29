5162b5224ed9684063f0900f03460d9174350470
Alexandro Perez <apgfriend@gmail.com>
2016-06-29 06:13:21 +0100

    add fix v3 for orientation changes

--- assets/orientation-listener.js
+++ assets/orientation-listener.js
@@ -6,66 +6,24 @@
 
 /*  Requirements: jQuery */
 
-var Device = function() {
-  // public properties
-  this.barHeight = screen.height - window.innerHeight;
-
-  // private variables
-  var orientation = getOrientation(screen.width, screen.height); // "landscape" or "portrait"
-  var onOrientationChangeFunction = function(){}; // function to execute when orientation changes
-
-  // privileged methods
-  this.getOrientation = function() {
-    return orientation;
-  };
-
-  this.addOrientationChangeListener = function(fn) {
-    onOrientationChangeFunction = fn;
-  };
-
-  this.updateState = function() {
-    if (orientation !== getOrientation(screen.width, screen.height)) {
-      orientation = getOrientation(screen.width, screen.height);
-      onOrientationChange(onOrientationChangeFunction);
-    }
-  };
-
-  // private methods
-  function onOrientationChange(fn) {
-    fn();
-  }
-
-  // construcor code (always executes at instantiation)
-  window.addEventListener("resize", function(){
-    device.updateState();
-  });
-  onOrientationChange();
-};
-
-// Useful functions
-function getOrientation(width, height) {
-  if (width > height){
-    return "landscape";
-  } else {
-    return "portrait";
-  }
-}
-function isMobileDevice() {
+var isMobileDevice = function() {
   var ua = navigator.userAgent.toLowerCase();
   var android = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
   var iPhone = ua.indexOf("iphone") > -1; //&& ua.indexOf("mobile");
   var iPad = ua.indexOf("ipad") > -1; //&& ua.indexOf("mobile");
   return android || iPhone || iPad;
-}
+}();
 
-var device = new Device();
+var barHeight = screen.height - window.innerHeight;
 
-device.addOrientationChangeListener(function() {
+var handleOrientationChange = function() {
   var height = "100vh";
-  if (isMobileDevice()) {
-    height = screen.height - device.barHeight;
+  if (isMobileDevice) {
+    height = screen.height - barHeight;
     height = height.toString() + "px";
   }
   $("header").css("height", height);
-  $("a").text(device.barHeight.toString());
-});
+};
+
+handleOrientationChange();
+window.addEventListener("orientationchange", handleOrientationChange);
