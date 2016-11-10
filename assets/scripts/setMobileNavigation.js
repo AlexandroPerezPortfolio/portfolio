4e971a0c96f8530eb0401b000bb811a7cd0e6d14
Alexandro Perez <apgfriend@gmail.com>
2016-11-10 23:44:11 +0000

    optimize navigation colors and jquery functionality for mobile devices

--- assets/scripts/setMobileNavigation.js
+++ assets/scripts/setMobileNavigation.js
@@ -8,15 +8,35 @@ function setMobileNavigation() {
 
   menu.click(function(e) {
     if (isHidden){
-      menuLinks.css("display", "block");
-      menu.html('<i class="fa fa-times" aria-hidden="true"></i>');
-      isHidden = false;
+      showMenu();
     } else {
-      menuLinks.css("display", "");
-      menu.html('<i class="fa fa-bars" aria-hidden="true"></i>');
-      isHidden = true;
+      hideMenu();
     }
   });
+
+  $("#menu-links, #menu-links > li > a").click(function(){
+    hideMenu();
+  });
+
+  $(".title").click(function(){
+    $('html, body').animate({
+        scrollTop: $("header").offset().top
+    }, 1000);
+  });
+
+  function showMenu() {
+    console.log("menu shown");
+    menuLinks.css("display", "block");
+    menu.html('<i class="fa fa-times" aria-hidden="true"></i>');
+    isHidden = false;
+  }
+
+  function hideMenu() {
+    console.log("menu hidden");
+    menuLinks.css("display", "");
+    menu.html('<i class="fa fa-bars" aria-hidden="true"></i>');
+    isHidden = true;
+  }
 
 }
 
